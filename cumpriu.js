03daac5d91a9c7fd589664d83b3a000032610f39
Gabriel Coutinho <bielg8998@gmail.com>
2026-01-31 11:34:28 -0300

    Upgrade completo 2.0

--- cumpriu.js
+++ cumpriu.js
@@ -2,15 +2,24 @@ document.querySelector("#btn").addEventListener("click", function () {
     let mesestrabalhados = Number(document.querySelector("#meses").value);
     let salario = Number(document.querySelector("#salario").value);
     let mesdesligado = Number(document.querySelector("#desligou").value);
+    if (mesdesligado > 12) {
+        alert(`O Mês do desligamento não pode ser maior que 12 meses!!`)
+        ferias = 0
+    }
     let dias_trabalhados = Number(document.querySelector("#dias").value)
+    if (dias_trabalhados > 31){
+        alert(`Dias trabalhados não pode ser maior que 31 dias`)
+        saldosalario = 0
+    }
 
-    let FGTS = (salario * 0.08) * mesestrabalhados + 0.4
-    let ferias = ((salario / 12) * mesdesligado) + 0.33;
+    let FGTS = ((salario * 0.08) * mesestrabalhados)
+    let resultfgts_porcentagem = FGTS * 0.4 + FGTS;
+    let ferias = ((salario / 12) * mesdesligado) + (salario * 0.33);
     let saldosalario = salario / 30 * dias_trabalhados
     let total = FGTS + ferias + salario
 
-    let saida_saldo_salario = document.querySelector("#saldosalario").innerHTML = `SALDO SALÁRIO: R$${saldosalario.toFixed(2)}`
-    let saida_ferias = document.querySelector("#ferias").innerHTML = `FERIAS: R$${ferias.toFixed(2)}`
-    let saida_fgts = document.querySelector("#fgts").innerHTML = `FGTS: R$${FGTS.toFixed(2)}`
-    let saida_total = document.querySelector("#total").innerHTML = `TOTAL: R$${total.toFixed(2)}`
+    let saida_saldo_salario = document.querySelector("#saldosalario").innerHTML = `SALDO SALÁRIO: R$${Math.round(saldosalario * 100) / 100}`
+    let saida_ferias = document.querySelector("#ferias").innerHTML = `FERIAS: R$${Math.round(ferias * 100) / 100}`
+    let saida_fgts = document.querySelector("#fgts").innerHTML = `FGTS: R$${Math.round(resultfgts_porcentagem * 100) / 100}`
+    let saida_total = document.querySelector("#total").innerHTML = `TOTAL: R$${Math.round(total * 100) / 100}`
 })
--- nãoqueremos.js
+++ nãoqueremos.js
@@ -1,19 +1,26 @@
-document.querySelector("#btn").addEventListener("click", function(){
-    document.querySelector("#btn").addEventListener("click", function () {
+document.querySelector("#btn").addEventListener("click", function () {
     let mesestrabalhados = Number(document.querySelector("#meses").value);
     let salario = Number(document.querySelector("#salario").value);
     let mesdesligado = Number(document.querySelector("#desligou").value);
+    if (mesdesligado > 12){
+        alert(`O mês do desligamento não pode ser maior que 12!!`)
+        ferias = 0
+    }
     let dias_trabalhados = Number(document.querySelector("#dias").value)
+    if(dias_trabalhados > 31){
+        alert(`Os dias trabalhados não podem ser maior que 31`)
+        saldosalario = 0
+    }
 
-    let FGTS = (salario * 0.08) * mesestrabalhados + 0.4
-    let ferias = ((salario / 12) * mesdesligado) + 0.33;
+    let FGTS = (salario * 0.08) * mesestrabalhados;
+    let resultfgts_porcentagem = FGTS * 0.4 + FGTS;
+    let ferias = ((salario / 12) * mesdesligado) + (salario * 0.33);
     let saldosalario = salario / 30 * dias_trabalhados
     let total = FGTS + ferias + salario
 
-    let saida_salario = document.querySelector("#reslt_salario").innerHTML = `SALÁRIO: R$${salario.toFixed()}`
-    let saida_saldo_salario = document.querySelector("#saldosalario").innerHTML = `SALDO SALÁRIO: R$${saldosalario.toFixed(2)}`
-    let saida_ferias = document.querySelector("#ferias").innerHTML = `FERIAS: R$${ferias.toFixed(2)}`
-    let saida_fgts = document.querySelector("#fgts").innerHTML = `FGTS: R$${FGTS.toFixed(2)}`
-    let saida_total = document.querySelector("#total").innerHTML = `TOTAL: R$${(total + salario).toFixed(2)}`
-})
+    let saida_salario = document.querySelector("#reslt_salario").innerHTML = `SALÁRIO: R$${Math.round(salario * 100) / 100}`
+    let saida_saldo_salario = document.querySelector("#saldosalario").innerHTML = `SALDO SALÁRIO: R$${Math.round(saldosalario * 100) / 100}`
+    let saida_ferias = document.querySelector("#ferias").innerHTML = `FERIAS: R$${Math.round(ferias * 100) / 100}`
+    let saida_fgts = document.querySelector("#fgts").innerHTML = `FGTS: R$${Math.round(resultfgts_porcentagem * 100) / 100}`
+    let saida_total = document.querySelector("#total").innerHTML = `TOTAL: R$${Math.round((total + salario) * 100) / 100}`
 })

9f29a921c7cdfb4c3b8bfa7a22173d3dd380d6b5
Gabriel Coutinho <bielg8998@gmail.com>
2026-01-30 23:06:05 -0300

    Rescisão CLT

---
+++ nãocumpriu.js
@@ -0,0 +1,19 @@
+document.querySelector("#btn").addEventListener("click", function(){
+    document.querySelector("#btn").addEventListener("click", function () {
+    let mesestrabalhados = Number(document.querySelector("#meses").value);
+    let salario = Number(document.querySelector("#salario").value);
+    let mesdesligado = Number(document.querySelector("#desligou").value);
+    let dias_trabalhados = Number(document.querySelector("#dias").value)
+
+    let FGTS = (salario * 0.08) * mesestrabalhados + 0.4
+    let ferias = ((salario / 12) * mesdesligado) + 0.33;
+    let saldosalario = salario / 30 * dias_trabalhados
+    let total = FGTS + ferias + salario
+
+    let saida_salario = document.querySelector("#reslt_salario").innerHTML = `SALÁRIO: R$${salario}`
+    let saida_saldo_salario = document.querySelector("#saldosalario").innerHTML = `SALDO SALÁRIO: R$${saldosalario}`
+    let saida_ferias = document.querySelector("#ferias").innerHTML = `FERIAS: R$${ferias}`
+    let saida_fgts = document.querySelector("#fgts").innerHTML = `FGTS: R$${FGTS}`
+    let saida_total = document.querySelector("#total").innerHTML = `TOTAL: R$${total - salario}`
+})
+})
